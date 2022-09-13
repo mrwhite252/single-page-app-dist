08bdb65cee89c32191a873689fa7d78231e1aaf3
David Bai <baiminghan@gmail.com>
2022-09-14 09:24:07 +1200

    Update main.js

--- main.js
+++ main.js
@@ -177,7 +177,7 @@ function interactiveMap() {
   const chcDepot = [-43.477002, 172.55427],
     zqnDepot = [-45.024246, 168.736876],
     vehicleIcon = L.icon({
-      iconUrl: "./svg/marker.svg",
+      iconUrl: "./SVG/marker.svg",
 
       iconSize: [30, 30],
     }),
